bac332560df6061195cbb19bb857ea2b814584bc
JBuck31 <jrbuck31@gmail.com>
2020-01-05 14:43:26 -0600

    created table that displays planet name, number of earth days and type

--- src/main/java/org/launchcode/enumerableplanets/data/Planets.java
+++ src/main/java/org/launchcode/enumerableplanets/data/Planets.java
@@ -2,23 +2,37 @@ package org.launchcode.enumerableplanets.data;
 
 public enum Planets {
 
-    MERCURY("Mercury"),
-    VENUS("Venus"),
-    EARTH("Earth"),
-    MARS("Mars"),
-    JUPITER("Jupiter"),
-    SATURN("Saturn"),
-    URANUS("Uranus"),
-    NEPTUNE("Neptune");
+    MERCURY("Mercury", 88, "rocky"),
+    VENUS("Venus", 225, "rocky"),
+    EARTH("Earth", 365, "rocky"),
+    MARS("Mars", 687, "rocky"),
+    JUPITER("Jupiter", 4333, "gas giant"),
+    SATURN("Saturn", 10759, "gas giant"),
+    URANUS("Uranus", 30687, "ice giant"),
+    NEPTUNE("Neptune", 60200, "ice giant");
 
     private final String name;
 
-    Planets(String name) {
+    private final int yearLength;
+
+    private final String type;
+
+    Planets(String name, int yearLength, String type) {
         this.name = name;
+        this.yearLength = yearLength;
+        this.type = type;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getYearLength() {
+        return yearLength;
+    }
+
+    public String getType() {
+        return type;
     }
 }
 
